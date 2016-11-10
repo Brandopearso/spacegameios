c42ed3f49faaaaa89c44c7947ca18368aee45423
Arjun Gopisetty <arjungopisetty@wireless-10-145-160-223.public.utexas.edu>
2016-11-10 08:57:27 -0600

    Twitter integration added to DeathVC

--- SpaceGame/DeathViewController.swift
+++ SpaceGame/DeathViewController.swift
@@ -9,12 +9,34 @@
 import Foundation
 import UIKit
 import SpriteKit
+import Social
+import Accounts
 
 class DeathViewController : UIViewController
 {
     override func viewDidLoad()
     {
         
+    }
+    
+    
+    @IBAction func sendTweetButtonHandler(sender: AnyObject) {
+        tweetVC()
+    }
+    
+    func tweetVC()
+    {
+        let highscore = NSUserDefaults.standardUserDefaults().valueForKey("Highscore") as! NSInteger!
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter){
+            let twitterController:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            twitterController.setInitialText("My high score in space game: " + String(highscore) + ". Beat my score!")
+            self.presentViewController(twitterController, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Twitter Account", message: "Please login to your Twitter account.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
 }
